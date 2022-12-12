6555af8f5a9df90704bcf075f5d3858376b47202
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-12 15:53:50 +0300

    PhoneBookItemAdd dto get-set improvement. Startup service AddTransient line added.

--- ContactMicroservice/Dtos/PhoneBookItemAddDto.cs
+++ ContactMicroservice/Dtos/PhoneBookItemAddDto.cs
@@ -4,13 +4,132 @@ namespace ContactMicroservice.Dtos
 {
     public class PhoneBookItemAddDto
     {
+        private string _name;
+        private string _surname;
+        private string _firm;
+        private string _phone;
+        private string _mail;
+        private string _country;
+        private string _city;
+
         public Guid Guid { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Firm { get; set; }
-        public string Phone { get; set; }
-        public string Mail { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
+        public string Name 
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _name = string.Empty;
+                }
+
+                _name = value.Trim();
+            }
+        }
+
+        public string Surname 
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _surname = string.Empty;
+                }
+
+                _surname = value.Trim();
+            }
+        }
+
+        public string Firm
+        {
+            get
+            {
+                return _firm;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _firm = string.Empty;
+                }
+
+                _firm = value.Trim();
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _phone = string.Empty;
+                }
+
+                _phone = value.Trim();
+            }
+        }
+
+        public string Mail
+        {
+            get
+            {
+                return _mail;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _mail = string.Empty;
+                }
+
+                _mail = value.Trim();
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _country = string.Empty;
+                }
+
+                _country = value.Trim();
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _city = string.Empty;
+                }
+
+                _city = value.Trim();
+            }
+        }
     }
 }
