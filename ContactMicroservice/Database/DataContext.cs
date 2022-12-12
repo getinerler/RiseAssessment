d32a92c665387e933afb4c4e7e8dbae47fbe890f
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-12 18:57:03 +0300

    Data layer created.

---
+++ ContactMicroservice/Database/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using ContactMicroservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactMicroservice.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<PhoneBookItem> PhoneBookItems { get; set; }
+    }
+}
