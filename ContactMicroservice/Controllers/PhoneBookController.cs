096a330f7da75cb1c74b8579b1ab123553319305
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-12 15:09:41 +0300

    PhoneBookController created.

---
+++ ContactMicroservice/Controllers/PhoneBookController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ContactMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PhoneBookController : ControllerBase
+    {
+        public PhoneBookController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Save()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, string key, string value)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
