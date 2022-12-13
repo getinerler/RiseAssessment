5186bfe7e133ee20789fdf90c30fb8d3bcea7fe5
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-13 16:48:02 +0300

    Models created.

---
+++ ReportMicroservice/Models/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReportMicroservice.Models
+{
+    public class Report
+    {
+        public int ReportId { get; set; }
+        public Guid Guid { get; set; }
+        public string Path { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
