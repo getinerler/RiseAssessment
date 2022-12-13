4d555a1fab43f292e8ed60add177699cf8340bb1
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-13 16:40:34 +0300

    ReportController created. WeatherForecast items removed.

---
+++ ReportMicroservice/Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ReportMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        public ReportController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Info(Guid guid)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
