d32c15f2ac99095ecbe45796ca3ae06b1f54a52e
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-14 16:23:15 +0300

    ContactMicroservice helper class created.

---
+++ ReportMicroservice/Helpers/ContactMicroserviceHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using ReportMicroservice.Dtos;
+using System.IO;
+using System.Net;
+
+namespace ReportMicroservice.Helpers
+{
+    public class ContactMicroserviceHelper
+    {
+        private readonly string url = "localhost:3000/ContactMicroservice/";
+
+        public ContactMicroserviceReportItem[] GetInfo()
+        {
+            string info = Get(url + "/GetReportInfo");
+
+            ContactMicroserviceReportItem[] items =
+                JsonConvert.DeserializeObject<ContactMicroserviceReportItem[]>(info);
+            return items;
+        }
+
+        private string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
