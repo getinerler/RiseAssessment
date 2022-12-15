ed61b3dfb9ca422af43c8a97ed0e88b6ff29a671
Güven Çetinerler <guven@infotecbilisim.com>
2022-12-15 11:14:44 +0300

    Report Microservice new migration.

---
+++ ReportMicroservice/Migrations/20221215081159_ColumnUpdates.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReportMicroservice.Migrations
+{
+    public partial class ColumnUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "Reports");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ExcelFileReady",
+                table: "Reports",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExcelFileReady",
+                table: "Reports");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "Reports",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
